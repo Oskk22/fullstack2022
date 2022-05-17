ed0712d835793129c37ecb76a5b1b8095dc50898
Oskk22 <105198327+Oskk22@users.noreply.github.com>
2022-05-17 14:01:16 +0200

    17/05

---
+++ JAVASCRIPT/variables.js
@@ -0,0 +1,33 @@
+// kebap-case (no permitido en JS)
+const last-name = 'Doe';
+// snake_case (recomendado en otros lenguajes como Phyton)
+const last_name = 'last_name';
+// PascalCase (recomendado en Js para las clases)
+const LastName ='LastName';
+// camelCase (recomendado en JS para las variables y funciones)
+const lastName = 'lastName';
+document.write (lastName);
+const person = 'Joe Doe', carName = 'Volvo', price = 200;
+let x; //Creando variable (indefinido)
+x = 5; //Asigna valor, de tipo numerico entero y valor 5
+x = "Texto"; //Asigna valor, de tipo string y valor Texto.
+const variable1 = 1/0; // valor infinity (solo operaciones con numeros)
+const variable2 = parseFloat('prueba'); //NaN (conversion incorrecta a numerica)
+let variable4;
+//undefined
+console.log(variable4);
+//error
+console.log(variable_nodeclarada);
+// VAR Y LET
+var alert1 ='5';
+//permitido
+var alert1 = '';
+let alert2 = 5;
+//no permitido porque ya se declaro antes
+// let alert2= 5;
+alert2 = 6  
+
+// EJERCICIO 164-165
+var a = '1';
+var b = '2';
+var c = '3';
