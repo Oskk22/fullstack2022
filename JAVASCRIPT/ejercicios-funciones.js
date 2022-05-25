afd7ff5c9d17ebff27354a9a212188c8d01bb5e7
Oskk22 <105198327+Oskk22@users.noreply.github.com>
2022-05-25 12:59:31 +0200

    25/05 1.0

---
+++ JAVASCRIPT/ejercicios-funciones.js
@@ -0,0 +1,29 @@
+//EJERCICIO 1 DECLARAR FUNCION FLECHA
+const sumar = (x,y) => {
+    return x + y;
+};
+const restar = (x,y) => {
+    return x - y;
+};
+const multiplicar = (x,y) => {
+    return x * y;
+};
+const dividir = (x,y) => {
+    return x / y;
+};
+
+console.log(sumar(1,2));
+console.log(restar(1,2));
+console.log(multiplicar(1,2));
+console.log(dividir(1,2));
+
+//EJERCICIO 2 DECLARAR FUNCION FLECHA SIMPLIFICADA
+const sumar = (x,y) => x + y;
+const restar = (x,y) => x - y;
+const multiplicar = (x,y) => x * y;
+const dividir = (x,y) => x / y;
+
+console.log(sumar(1,2));
+console.log(restar(1,2));
+console.log(multiplicar(1,2));
+console.log(dividir(1,2));
