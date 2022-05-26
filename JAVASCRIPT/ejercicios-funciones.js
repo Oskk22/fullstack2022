cc6694ff62d6f17d8573e101b9ff0fcc4a7030c4
Oskk22 <105198327+Oskk22@users.noreply.github.com>
2022-05-26 13:56:40 +0200

    26/05

--- JAVASCRIPT/ejercicios-funciones.js
+++ JAVASCRIPT/ejercicios-funciones.js
@@ -27,3 +27,44 @@ console.log(sumar(1,2));
 console.log(restar(1,2));
 console.log(multiplicar(1,2));
 console.log(dividir(1,2));
+
+//setTimeout y setInterval funciones asincronas
+setTimeout(() => {
+    console.log('Esta funcion se ejecuta despues de 5 segundos')
+}, 5000);
+
+setInterval(() => {
+    console.log('Esta funcion se ejecuta cada 5 segundos')
+}; 5000);
+
+
+//Anidacion de dunciones asincronas
+setTimeout(() => {
+    console.log('Hola');
+    setTimeout(() => {
+        console.log('Adios');
+    }; 500);
+}; 1000)
+console.log('Buenos dias'); 
+
+//EJERCICIO 5-178PAG
+setTimeout(() => {
+    console.log(sumar(4,2, (resultado) => {
+        console.log(resultado)
+    }));
+}, 1000);
+setTimeout(() => {
+    console.log(restar(3,1, (resultado) => {
+        console.log(resultado)
+    }));
+}, 2000);
+setTimeout(() => {
+    console.log(multiplicar(5,7, (resultado) => {
+        console.log(resultado)
+    }));
+}, 3000);
+setTimeout(() => {
+    console.log(dividir(6,8, (resultado) => {
+        console.log(resultado)
+    }));
+}, 4000);
